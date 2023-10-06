3b21f771a00c6eca119e463959057f34dde514e3
Thomas Lemay <2230852@champlaincollege.qc.ca>
2023-10-06 12:37:40 -0400

    Add project files.

---
+++ WordUnscrambler/WordMatcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class WordMatcher
+    {
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        {
+            List<MatchedWord> matchedWords = new List<MatchedWord>();
+
+            foreach (string scrambleword in scrambledWords)
+            {
+                foreach (string word in wordList)
+                {
+                    char[] char1 = scrambleword.ToCharArray();
+                    char[] char2 = word.ToCharArray();
+
+
+                    if (char1.SequenceEqual(char2))
+                    {
+                        MatchedWord matchedWord = new MatchedWord
+                        {
+                            ScrambledWord = scrambleword,
+                            Word = word
+                        };
+                        matchedWords.Add(matchedWord);
+
+                    }
+
+
+                }
+                return matchedWords;
+            }
+
+            // Work with "scrambledWords" and "matchedWords".
+
+            MatchedWord BuildMatchedWord(string scrambledWord, string word)
+            {
+                // Build a matched-word object here, so that you can return it.
+
+                //return matchedWord;
+                return new MatchedWord();  // Delete this line when done.
+            }
+
+            return matchedWords;
+        }
+    }
+
+}
+
