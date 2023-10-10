455db2abaa0d516a29921c9f61a640eb901afbbb
Thomas Lemay <2230852@champlaincollege.qc.ca>
2023-10-10 00:43:54 -0400

    ExtensionMethods fixed and completed

--- WordUnscrambler/ExtensionMethods.cs
+++ WordUnscrambler/ExtensionMethods.cs
@@ -15,32 +15,43 @@ namespace WordUnscrambler
         {
             var filename = Console.ReadLine();
             string[] scrambledWords = _fileReader.Read(filename);
-            DisplayMatchedUnscrambledWords(scrambledWords);
+            DisplayMatchedUnscrambledWords(scrambledWords, null);
         }
 
         public static void ExecuteScrambledWordsManualEntryScenario()
         {
             Console.WriteLine("Enter word(s) manually (separated by commas if multiple): ");
-            string[] wordArray = Console.ReadLine().Split(',');
+            string[] wordsArray = Console.ReadLine().Split(',');
 
             Console.WriteLine("Enter scrambled word(s) manually(separated by commas if multiple): ");
             string[] scrambledWordArray = Console.ReadLine().Split(',');
 
-
+            DisplayMatchedUnscrambledWords(scrambledWordArray, wordsArray);
         }
 
 
-        public static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        public static void DisplayMatchedUnscrambledWords(string[] scrambledWords, string[] wordsArray)
         {
-            //read the list of words from the system file. 
-            string[] wordList = _fileReader.Read("wordlist.txt");
 
-            //call a word matcher method to get a list of structs of matched words.
-            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+            List<MatchedWord> matchedWords = new List<MatchedWord>();
+            
+            if(wordsArray is null)
+            {
+                //read the list of words from the system file. 
+                string[] wordList = _fileReader.Read("unscrambledWords.txt");
+
+                //call a word matcher method to get a list of structs of matched words.
+                matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+            }
+
+            else
+            {
+                matchedWords = _wordMatcher.Match(scrambledWords, wordsArray);
+            }
 
             foreach (MatchedWord word in matchedWords)
             {
-                Console.WriteLine($"scrambledWord :   {word.ScrambledWord}   matched with the word :   {word.Word}");
+                Console.WriteLine($"scrambledWorld :  {word.ScrambledWord}  matched with the word :  {word.Word}");
             }
         }
     }
